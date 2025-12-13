b6ce8c49acb7c61de4f12fa8c46185b94fe97452
عبدالله الرهراني <Abohoosksa80@gmail.com>
2025-12-13 12:50:47 +0300

    update the button

--- financial-freedom-c/RetirementPlanForm.Designer.cs
+++ financial-freedom-c/RetirementPlanForm.Designer.cs
@@ -35,23 +35,23 @@
             this.cmbYears = new System.Windows.Forms.ComboBox();
             this.txtResult = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label3 = new System.Windows.Forms.Label();
             this.lblMonthlySavingValue = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnExit
             // 
             this.btnExit.BackColor = System.Drawing.Color.Red;
-            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExit.Location = new System.Drawing.Point(869, 380);
+            this.btnExit.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(935, 516);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(75, 40);
+            this.btnExit.Size = new System.Drawing.Size(143, 64);
             this.btnExit.TabIndex = 0;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = false;
@@ -60,10 +60,10 @@
             // btnCalculate
             // 
             this.btnCalculate.BackColor = System.Drawing.Color.LightSeaGreen;
-            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCalculate.Location = new System.Drawing.Point(420, 382);
+            this.btnCalculate.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(451, 516);
             this.btnCalculate.Name = "btnCalculate";
-            this.btnCalculate.Size = new System.Drawing.Size(147, 36);
+            this.btnCalculate.Size = new System.Drawing.Size(142, 61);
             this.btnCalculate.TabIndex = 1;
             this.btnCalculate.Text = "Calculate";
             this.btnCalculate.UseVisualStyleBackColor = false;
@@ -71,10 +71,10 @@
             // 
             // btnBack
             // 
-            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBack.Location = new System.Drawing.Point(39, 382);
+            this.btnBack.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.Location = new System.Drawing.Point(17, 527);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(84, 36);
+            this.btnBack.Size = new System.Drawing.Size(138, 53);
             this.btnBack.TabIndex = 2;
             this.btnBack.Text = "Back";
             this.btnBack.UseVisualStyleBackColor = true;
@@ -124,9 +124,9 @@
             "38",
             "39",
             "40"});
-            this.cmbGrowth.Location = new System.Drawing.Point(299, 269);
+            this.cmbGrowth.Location = new System.Drawing.Point(321, 349);
             this.cmbGrowth.Name = "cmbGrowth";
-            this.cmbGrowth.Size = new System.Drawing.Size(121, 24);
+            this.cmbGrowth.Size = new System.Drawing.Size(106, 24);
             this.cmbGrowth.TabIndex = 3;
             // 
             // cmbYears
@@ -148,68 +148,57 @@
             "13",
             "14",
             "15"});
-            this.cmbYears.Location = new System.Drawing.Point(256, 196);
+            this.cmbYears.Location = new System.Drawing.Point(283, 276);
             this.cmbYears.Name = "cmbYears";
-            this.cmbYears.Size = new System.Drawing.Size(121, 24);
+            this.cmbYears.Size = new System.Drawing.Size(106, 24);
             this.cmbYears.TabIndex = 4;
             // 
             // txtResult
             // 
-            this.txtResult.BackColor = System.Drawing.Color.Yellow;
-            this.txtResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtResult.Location = new System.Drawing.Point(16, 13);
+            this.txtResult.BackColor = System.Drawing.Color.Linen;
+            this.txtResult.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Location = new System.Drawing.Point(14, 13);
             this.txtResult.Multiline = true;
             this.txtResult.Name = "txtResult";
             this.txtResult.ReadOnly = true;
-            this.txtResult.Size = new System.Drawing.Size(370, 181);
+            this.txtResult.Size = new System.Drawing.Size(324, 181);
             this.txtResult.TabIndex = 5;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(18, 196);
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(75, 276);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(204, 25);
+            this.label1.Size = new System.Drawing.Size(204, 23);
             this.label1.TabIndex = 6;
             this.label1.Text = "Years Until Retirement";
-            // 
-            // label
-            // 
-            this.label.AutoSize = true;
-            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label.Location = new System.Drawing.Point(272, 133);
-            this.label.Name = "label";
-            this.label.Size = new System.Drawing.Size(207, 22);
-            this.label.TabIndex = 7;
-            this.label.Text = "Monthly Saving Capacity";
-            this.label.Click += new System.EventHandler(this.lblMonthlySavingValue_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(3, 269);
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(47, 349);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(273, 25);
+            this.label2.Size = new System.Drawing.Size(268, 23);
             this.label2.TabIndex = 8;
             this.label2.Text = "Expected Annual Growth Rate";
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.txtResult);
-            this.panel1.Location = new System.Drawing.Point(485, 120);
+            this.panel1.Location = new System.Drawing.Point(483, 200);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(401, 214);
+            this.panel1.Size = new System.Drawing.Size(351, 214);
             this.panel1.TabIndex = 9;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(644, 92);
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(623, 172);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(76, 25);
+            this.label3.Size = new System.Drawing.Size(71, 23);
             this.label3.TabIndex = 6;
             this.label3.Text = "Results";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -217,10 +206,10 @@
             // lblMonthlySavingValue
             // 
             this.lblMonthlySavingValue.AutoSize = true;
-            this.lblMonthlySavingValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMonthlySavingValue.Location = new System.Drawing.Point(18, 130);
+            this.lblMonthlySavingValue.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMonthlySavingValue.Location = new System.Drawing.Point(63, 212);
             this.lblMonthlySavingValue.Name = "lblMonthlySavingValue";
-            this.lblMonthlySavingValue.Size = new System.Drawing.Size(235, 25);
+            this.lblMonthlySavingValue.Size = new System.Drawing.Size(228, 23);
             this.lblMonthlySavingValue.TabIndex = 10;
             this.lblMonthlySavingValue.Text = "Monthly Saving Capacity:";
             this.lblMonthlySavingValue.Click += new System.EventHandler(this.lblMonthlySavingValue_Click_1);
@@ -228,30 +217,40 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(395, 24);
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(424, 29);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(191, 29);
+            this.label4.Size = new System.Drawing.Size(215, 32);
             this.label4.TabIndex = 11;
             this.label4.Text = "Retirement Plan ";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(89, 53);
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(38, 116);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(888, 25);
+            this.label5.Size = new System.Drawing.Size(1026, 25);
             this.label5.TabIndex = 12;
             this.label5.Text = "Plan your expected retirement savings based on your monthly saving capacity and i" +
     "nvestment growth.";
             // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(297, 213);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(0, 22);
+            this.label.TabIndex = 7;
+            this.label.Click += new System.EventHandler(this.lblMonthlySavingValue_Click);
+            // 
             // RetirementPlanForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.SeaGreen;
-            this.ClientSize = new System.Drawing.Size(1036, 450);
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1090, 608);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.lblMonthlySavingValue);
@@ -265,6 +264,7 @@
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.btnCalculate);
             this.Controls.Add(this.btnExit);
+            this.ForeColor = System.Drawing.SystemColors.GrayText;
             this.Name = "RetirementPlanForm";
             this.Text = "RetirementPlanForm";
             this.Load += new System.EventHandler(this.RetirementPlanForm_Load);
@@ -284,12 +284,12 @@
         private System.Windows.Forms.ComboBox cmbYears;
         private System.Windows.Forms.TextBox txtResult;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label lblMonthlySavingValue;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label;
     }
 }
